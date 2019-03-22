87f3f2611eb8f0668ed0f64028de85059410660a
Natoria Ray <natoriaray@gmail.com>
2019-03-21 21:05:24 -0500

    testing out button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById('calbtn').addEventListener('click', function() {
+
+  var calcInfo = {
+      billAmount: document.getElementById('bill_amount').value,
+      numOfPeople: document.getElementById('num_people').value
+    }
   
+
+  console.log(calcInfo);
 })
