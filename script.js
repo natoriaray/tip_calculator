1b340902b95ea40181f3c49cadd488a85b535b50
Natoria Ray <natoriaray@gmail.com>
2019-04-19 01:24:46 -0500

    made variable that stores the data from the input fields

--- script.js
+++ script.js
@@ -6,25 +6,67 @@ var calculateController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+  var DOMstrings = {
+    inputBillAmount: '.bill_input',
+    inputExperience: '.experience_input',
+    inputNumberOfPeople: '.num_people',
+    button: '.cal_btn'
+  };
+
+
+  return {
+    getInput: function() {
+      return {
+        billAmount: document.querySelector(DOMstrings.inputBillAmount).value,
+        experience: document.querySelector(DOMstrings.inputExperience).value,
+        numberOfPeople: document.querySelector(DOMstrings.inputNumberOfPeople).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    },
+
+    testing: function() {
+      return this.getInput();
+    }
+  };
 })();
 
 // GLOBAL APP CONTROLLER
-var controller = (function() {
+var controller = (function(calcCtrl, UICtrl) {
 
   var setupEventListeners = function() {
 
-    document.querySelector('.calbtn').addEventListener('click', calculateTip);
+    var DOM = UICtrl.getDOMstrings();
 
+    document.querySelector(DOM.button).addEventListener('click', calculateTip);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keycode === 13 || event.which === 13) {
+        calculateTip();
+      }
+    });
   };
 
   var calculateTip = function() {
+    var input;
 
     // 1. Get input from fields
-
+    input = UICtrl.getInput();
+    
     // 2. Calculate tip for each person
 
     // 3. Display the tip on UI
 
   };
 
-})();
+  return {
+    init: function() {
+      setupEventListeners();
+    }
+  }
+
+})(calculateController, UIController);
+
+controller.init();
